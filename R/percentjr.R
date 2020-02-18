9d3e19ccfa539bf7e650928ce7e3c554f3d7fc4a
Julian <holidayjulian@mailbox.org>
2020-02-18 09:15:23 +0100

    Corrected spelling

--- R/percentjr.R
+++ R/percentjr.R
@@ -3,7 +3,7 @@
 #' @param x A numeric vector to format.
 #' @param accuracy A number to round to. Use (e.g.) `0.01` to show 2 decimal places of precision. If `NULL`, the default, uses a heuristic that should ensure breaks have the minimum number of digits needed to show the difference between adjacent values.
 #' Applied to rescaled data.
-#' @param scale A scaling factor: x will be multiplied by `scale` before formating. This is useful if the underlying data is very small or very large.
+#' @param scale A scaling factor: x will be multiplied by `scale` before formatting. This is useful if the underlying data is very small or very large.
 #' @param prefix Symbols to display before and after value.
 #' @param suffix Symbols to display before and after value. If `suffix=="thinspace"` returns " %", else if `suffix` is a character, returns `suffix`. If it is not a character, returns "%".
 #'
@@ -12,7 +12,7 @@
 #' @param trim Logical, if `FALSE`, values are right-justified to a common width (see [base::format()]).
 #' @param ... Other arguments passed on to [base::format()].
 #'
-#' @return  Formated percentages
+#' @return  Formatted percentages
 #' @importFrom scales number
 #'
 #' @examples
