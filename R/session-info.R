3a022c35ac6ea2ce1be9b7da47210b321c9438aa
Julian <holidayjulian@mailbox.org>
2020-11-20 17:52:56 +0100

    Added a switch for checking pandoc version (parser and crossref)

--- R/session-info.R
+++ R/session-info.R
@@ -19,9 +19,15 @@
 #'   Examples: `CRAN (R 3.3.2)`, `Github (r-lib/pkgbuild@8aab60b)`,
 #'   `Bioconductor`, `local`.
 #'
+#' If the Pandoc version used to build the document is different from the version which was used
+#' to compile pandoc-crossref, this is noted in the output if `check_versions = TRUE`. This may be helpful
+#' because mixing versions is not supported and "strange things may (and likely will) happen silently"
+#' due to internal changes of Pandoc between versions.
+#'
 #' See [sessioninfo::package_info()] for the list of columns in the data frame that
 #' is *returned* (as opposed to *printed*).
 #' @param include_libs Include the library path of each package in the output. Disabled by default
+#' @param check_versions Compares the used version of Pandoc with the Pandoc version used to build pandoc-crossref. For more information, see Details
 #'
 #'
 #' @inheritParams sessioninfo::session_info
@@ -33,7 +39,7 @@
 #' Session_Info()
 #' Session_Info("jRmisc")
 
-Session_Info <- function(pkgs = NULL, include_base = FALSE, include_libs = FALSE) {
+Session_Info <- function(pkgs = NULL, include_base = FALSE, include_libs = FALSE, check_versions = TRUE) {
   info <- sessioninfo::session_info()
   if (!is.null(knitr::opts_knit$get('rmarkdown.pandoc.to'))) {
     info$platform$pandoc <- paste("Version", as.character(rmarkdown:::pandoc_version()))
@@ -44,10 +50,12 @@ Session_Info <- function(pkgs = NULL, include_base = FALSE, include_libs = FALSE
       info$platform$`pandoc-crossref` <- paste0("Version ", as.character(crossref_version()), " (Pandoc ", as.character(crossref_pandoc_version()), ")")
     }
   }
-  if (rmarkdown:::pandoc_version() != crossref_pandoc_version()) {
+  if (check_versions == TRUE) {
+    if (rmarkdown:::pandoc_version() != crossref_pandoc_version()) {
     info$platform$` `   <- "**********************************************"
     info$platform$`  `  <- "Versions Mismatch (Crossref and Pandoc)"
     info$platform$`   ` <- "**********************************************"
+    }
   }
   if (include_libs == FALSE) {
     class(info$packages) <- "Packages_Info"
