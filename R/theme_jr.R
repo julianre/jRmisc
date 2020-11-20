20d8d7ce2f2921cf83f241f7d8bd87c791e7d2f0
Julian <holidayjulian@mailbox.org>
2020-11-20 15:26:01 +0100

    Change standard font size

--- R/theme_jr.R
+++ R/theme_jr.R
@@ -13,7 +13,7 @@
 #' require(ggplot2)
 #' ggplot(df, aes(x = x, y = y)) + geom_point() + theme_jr()
 #' @export
-theme_jr <- function(base_size = 10, base_family = "",
+theme_jr <- function(base_size = 11, base_family = "",
                      base_line_size = base_size/22, base_rect_size = base_size/22) {
   half_line <- base_size/2
   theme(
